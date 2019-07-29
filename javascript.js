51758165b560ba10c64b4ade76acc01f5c98edc9
ines uwase <uwaseines10@gmail.com>
2019-07-29 10:07:26 +0200

    i have added new  script

--- javascript.js
+++ javascript.js
@@ -9,5 +9,16 @@
     // $("#design").toggle();
     $("#design-text").toggle();
   });
-
+  $("#message").click(function(event){
+    var name=$("#inputName").val();
+    var email=$("#inputEmail").val();
+    var message=$("#inputMessage").val();
+    if((name==="")||(email==="")||(message==="")){
+        alert("Please, fill the required fields!");
+    }
+    else{
+        alert(name+ ", we have received your message. Thank you for reaching out to us!");
+    }
+    event.preventDefault();
+});
 });
