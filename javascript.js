e23d2aa9a036229cc5104b8636cafd429891a601
ines uwase <uwaseines10@gmail.com>
2019-07-29 10:00:42 +0200

    i have used toggle

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function(){
+    $("#design").click(function(){
+      $("#design-text").toggle();
+      $("#design").toggle();
+    });
+  $("#design-text").click(function(){
+    $("#design").toggle();
+    // $("#design").toggle();
+    $("#design-text").toggle();
+  });
+
+});
